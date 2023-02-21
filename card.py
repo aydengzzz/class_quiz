1fb11cc3fba5d419213fd427db4455fea0f2a341
aydengzzz <121195820+aydengzzz@users.noreply.github.com>
2023-02-21 20:03:38 +0000

    Submit quiz solution

--- card.py
+++ card.py
@@ -1,6 +1,17 @@
-# Name:
+# Name: Ayden Rivera
 
 class Card:
     """class representing a simple Neapolitan card. Holds a 
     value and a suit"""
-    
+def __init__(self):
+    Card = (int(range)(1,11))
+def __repr__(self)
+    return F"Card(card = {1,11})"
+def __str__(self):
+    return str(Card(3,"diamonds"))
+def __eq__(self):
+    return F"Card(Card = self)"
+def __add__(card + other):
+    If card in card is self 
+    return "True"
+    print(card): 
